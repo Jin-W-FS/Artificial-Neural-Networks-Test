c171bd3917060a41bdc733936bd503eeb353bb72
Jin.W.FS <Jin.W.FSeer@gmail.com>
2011-02-07 11:12:55 +0800

    Homework 1, kernel, complete.

--- test.c
+++ test.c
@@ -2,15 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <assert.h>
 
 #include "NeuralLayer.h"
 
-/* train: y = (sin(2 * pi * x) + 1)/2, x in [0, 1) */
-#define func(x) ((sin(2 * 3.14159265 * (x)) + 1) / 2)
+#ifndef PI
+#	define PI 3.14159265
+#endif
+
+/* do a liner adjust: from in_sect[] to out_sect[]*/
+float SECTION_0_1[2] = { 0.0f, 1.0f };
+float adjust(float x, float in_sect[2], float out_sect[2]);
+
+/*
+  train: y = (sin(x) + cos(x)) / 2, -pi <= x <= pi,
+  x0 = (x + pi) / 2pi, 0 <= x0 <= 1,
+  y0 = (y + 1) / 2, 0 <= y0 <=1
+*/
+
+/* y = func(x) */
+float func(float x);
+float input_sect[2] = { -PI, PI };
+float output_sect[2] = { -1, 1 };
+
 #define N_SAMPLES 13
-#define SUM_ERROR 0.01
+#define SUM_ERROR 0.001
 
 const char* save_net = "./NeuralNetwork.log";
+#define ERROR_OUTPUT stdout
+#define TEST_INPUT stdin
+#define TEST_OUTPUT stdout
+
+/* x => x0 */
+float in_adj_rev(float x)
+{
+	return adjust(x, input_sect, SECTION_0_1);
+}
+/* x0 => x */
+float in_adj(float x0){
+	return adjust(x0, SECTION_0_1, input_sect);
+}
+/* y => y0 */
+float out_adj_rev(float y){
+	return adjust(y, output_sect, SECTION_0_1);
+}
+/* y0 => y */
+float out_adj(float y0){
+	return adjust(y0, SECTION_0_1, output_sect);
+}
 
 void gen_samples(float input[N_SAMPLES], float result[N_SAMPLES])
 {
@@ -18,8 +57,8 @@ void gen_samples(float input[N_SAMPLES], float result[N_SAMPLES])
 	float dim = 1.0 / (N_SAMPLES - 1);
 	for (i = 0; i < N_SAMPLES; i++)
 	{
-		input[i] = dim * i;
-		result[i] = func(input[i]);
+		input[i] = in_adj(dim * i);
+		result[i] = out_adj_rev(func(input[i]));
 	}
 }
 
@@ -29,7 +68,7 @@ int main(int argc, char* argv[])
 	int n_nodes[] = {
 		1, 23, 1
 	};
-	float input, result, error, sumerr;
+	float input, error, sumerr;
 	int i, n = 0;
 
 	float sample_input[N_SAMPLES];
@@ -64,22 +103,15 @@ int main(int argc, char* argv[])
 		{
 			sumerr += evolveNet(&net, &sample_input[i], &sample_result[i]);
 		}
-		printf("G %d : sum err = %f\n", n++, sumerr);
+		/* print Generation & errors */
+		fprintf(ERROR_OUTPUT, "%d\t%f\n", n++, sumerr);
 	}while(sumerr > SUM_ERROR);
-
-	/* print sample */
-	printf("\nnow start a test:\n samples:\n");
-	for (i = 0; i < N_SAMPLES; i++)
-	{
-		printf("sin(2 * pi * %f) = %f\n", sample_input[i], sample_result[i] * 2 - 1);
-	}
-
+	
 	/* run test */
-	while(scanf("%f", &input) == 1)
+	while(fscanf(TEST_INPUT, "%f", &input) == 1)
 	{
 		caculateNet(&net, &input);
-		result = net.output->result[0];
-		printf("sin(2 * pi * %f) = %f\n", input, result * 2 - 1);
+		fprintf(TEST_OUTPUT, "%f\n", out_adj(net.output->result[0]));
 	}
 
 	/* print & save Net */
@@ -92,5 +124,18 @@ int main(int argc, char* argv[])
 	releaseNet(&net);
 	
 	return 0;
+}
+
+float func(float x)
+{
+	return ((sin(x) + cos(x)) / 2);
+}
+
+float adjust(float x, float in_sect[2], float out_sect[2])
+{
+	assert(in_sect[1] - in_sect[0] != 0);
+	return out_sect[0] +					\
+		(x - in_sect[0]) / (in_sect[1] - in_sect[0]) *	\
+		(out_sect[1] - out_sect[0]);
 }
 
