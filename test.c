651032ac9f858191755366cc31e2b58dff03c81e
Jin.W.FS <Jin.W.FSeer@gmail.com>
2011-02-07 12:18:39 +0800

    Homework 2, kernel complete.

--- test.c
+++ test.c
@@ -12,30 +12,13 @@
 
 /* do a liner adjust: from in_sect[] to out_sect[]*/
 float SECTION_0_1[2] = { 0.0f, 1.0f };
+extern float input_sect[2];
+extern float output_sect[2];
+
 float adjust(float x, float in_sect[2], float out_sect[2]);
 
-/*
-  train: y = (sin(x) + cos(x)) / 2, -pi <= x <= pi,
-  x0 = (x + pi) / 2pi, 0 <= x0 <= 1,
-  y0 = (y + 1) / 2, 0 <= y0 <=1
-*/
-
-/* y = func(x) */
-float func(float x);
-float input_sect[2] = { -PI, PI };
-float output_sect[2] = { -1, 1 };
-
-#define N_SAMPLES 13
-#define SUM_ERROR 0.001
-
-const char* save_net = "./NeuralNetwork.log";
-#define ERROR_OUTPUT stdout
-#define TEST_INPUT stdin
-#define TEST_OUTPUT stdout
-
 /* x => x0 */
-float in_adj_rev(float x)
-{
+float in_adj_rev(float x){
 	return adjust(x, input_sect, SECTION_0_1);
 }
 /* x0 => x */
@@ -51,29 +34,46 @@ float out_adj(float y0){
 	return adjust(y0, SECTION_0_1, output_sect);
 }
 
-void gen_samples(float input[N_SAMPLES], float result[N_SAMPLES])
-{
-	int i;
-	float dim = 1.0 / (N_SAMPLES - 1);
-	for (i = 0; i < N_SAMPLES; i++)
-	{
-		input[i] = in_adj(dim * i);
-		result[i] = out_adj_rev(func(input[i]));
-	}
-}
+/* Infomation of the net */
+/*
+  train: y = (sin(x) + cos(x)) / 2, -pi <= x <= pi,
+  x0 = (x + pi) / 2pi, 0 <= x0 <= 1,
+  y0 = (y + 1) / 2, 0 <= y0 <=1
+*/
+
+/* y = func(x) */
+float input_sect[2] = { 0, 1 };
+float output_sect[2] = { 0, 1 };
+
+#define N_SAMPLES 8
+#define N_INPUTS  9
+#define N_OUTPUTS 1
+#define N_HIDDEN  (N_INPUTS + (int)(0.618 * (N_INPUTS - N_OUTPUTS) + 1))
+
+#define SUM_ERROR 0.001
+
+int n_nodes[] = {
+	N_INPUTS, N_HIDDEN, N_OUTPUTS
+};
+
+const char* save_net = "./NeuralNetwork.log";
+#define ERROR_OUTPUT stdout
+#define TEST_INPUT stdin
+#define TEST_OUTPUT stdout
+
+void gen_samples(float input[N_SAMPLES][N_INPUTS], float result[N_SAMPLES][N_OUTPUTS]);
+int read_input(float input[N_INPUTS], FILE* in);
+int write_output(float output[N_OUTPUTS], FILE* out);
 
 int main(int argc, char* argv[])
 {
 	NeuralNet net;
-	int n_nodes[] = {
-		1, 23, 1
-	};
-	float input, error, sumerr;
+	float error, sumerr;
 	int i, n = 0;
-
-	float sample_input[N_SAMPLES];
-	float sample_result[N_SAMPLES];
 	
+	float sample_input[N_SAMPLES][N_INPUTS];
+	float sample_result[N_SAMPLES][N_OUTPUTS];
+	float test_input[N_INPUTS];
 	FILE* fl = NULL;
 
 	/* init samples */
@@ -101,17 +101,17 @@ int main(int argc, char* argv[])
 		sumerr = 0;
 		for (i = 0; i < N_SAMPLES; i++)
 		{
-			sumerr += evolveNet(&net, &sample_input[i], &sample_result[i]);
+			sumerr += evolveNet(&net, sample_input[i], sample_result[i]);
 		}
 		/* print Generation & errors */
 		fprintf(ERROR_OUTPUT, "%d\t%f\n", n++, sumerr);
 	}while(sumerr > SUM_ERROR);
 	
 	/* run test */
-	while(fscanf(TEST_INPUT, "%f", &input) == 1)
+	while(read_input(test_input, stdin) == N_INPUTS)
 	{
-		caculateNet(&net, &input);
-		fprintf(TEST_OUTPUT, "%f\n", out_adj(net.output->result[0]));
+		caculateNet(&net, test_input);
+		write_output(net.output->result, stdout);
 	}
 
 	/* print & save Net */
@@ -137,5 +137,51 @@ float adjust(float x, float in_sect[2], float out_sect[2])
 	return out_sect[0] +					\
 		(x - in_sect[0]) / (in_sect[1] - in_sect[0]) *	\
 		(out_sect[1] - out_sect[0]);
+}
+
+const float sample_in[N_SAMPLES][N_INPUTS] =
+{
+	{ 1, 1, 1, 0, 1, 0, 0, 1, 0 },
+	{ 1, 0, 0, 1, 1, 1, 1, 0, 0 },
+	{ 0, 1, 0, 0, 1, 0, 1, 1, 1 },
+	{ 0, 0, 1, 1, 1, 1, 0, 0, 1 },
+	
+	{ 1, 0, 0, 1, 0, 0, 1, 1, 1 },
+	{ 0, 0, 1, 0, 0, 1, 1, 1, 1 },
+	{ 1, 1, 1, 0, 0, 1, 0, 0, 1 },
+	{ 1, 1, 1, 1, 0, 0, 1, 0, 0 }
+};
+const float sample_out[N_SAMPLES][N_OUTPUTS] =
+{
+	0, 0, 0, 0,
+	1, 1, 1, 1
+};
+
+void gen_samples(float input[N_SAMPLES][N_INPUTS], float result[N_SAMPLES][N_OUTPUTS])
+{
+	memcpy(&input[0][0], &sample_in[0][0], N_SAMPLES * N_INPUTS * sizeof(float));
+	memcpy(&result[0][0], &sample_out[0][0], N_SAMPLES * N_OUTPUTS * sizeof(float));
+}
+int read_input(float input[N_INPUTS], FILE* in)
+{
+	int i;
+	for (i = 0; i < N_INPUTS; i++)
+	{
+		if (fscanf(in, "%f", &input[i]) != 1)
+			break;
+	}
+	return i;
+}
+
+int write_output(float output[N_OUTPUTS], FILE* out)
+{
+	int i;
+	for (i = 0; i < N_OUTPUTS; i++)
+	{
+		if (fprintf(out, "%f ", output[i]) < 0)
+			break;
+	}
+	fputc('\n', out);
+	return i;
 }
 
