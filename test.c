e04d6c6ab36e5047749aa9318343c7d76361d2b3
Jin.W.FS <Jin.W.FSeer@gmail.com>
2011-02-14 20:45:56 +0800

    use cli opinions, print error, net-inf, test-result into separate files.

--- test.c
+++ test.c
@@ -4,6 +4,9 @@
 #include <math.h>
 #include <assert.h>
 
+/* posix: getopt */
+#include <unistd.h>
+
 #include "NeuralLayer.h"
 
 #ifndef PI
@@ -12,26 +15,10 @@
 
 /* do a liner adjust: from in_sect[] to out_sect[]*/
 float SECTION_0_1[2] = { 0.0f, 1.0f };
+extern float input_sect[2];
+extern float output_sect[2];
+
 float adjust(float x, float in_sect[2], float out_sect[2]);
-
-/*
-  train: y = (sin(x) + cos(x)) / 2, -pi <= x <= pi,
-  x0 = (x + pi) / 2pi, 0 <= x0 <= 1,
-  y0 = (y + 1) / 2, 0 <= y0 <=1
-*/
-
-/* y = func(x) */
-float func(float x);
-float input_sect[2] = { -PI, PI };
-float output_sect[2] = { -1, 1 };
-
-#define N_SAMPLES 13
-#define SUM_ERROR 0.001
-
-const char* save_net = "./NeuralNetwork.log";
-#define ERROR_OUTPUT stdout
-#define TEST_INPUT stdin
-#define TEST_OUTPUT stdout
 
 /* x => x0 */
 float in_adj_rev(float x)
@@ -51,44 +38,80 @@ float out_adj(float y0){
 	return adjust(y0, SECTION_0_1, output_sect);
 }
 
-void gen_samples(float input[N_SAMPLES], float result[N_SAMPLES])
+/*
+  train: y = (sin(x) + cos(x)) / 2, -pi <= x <= pi,
+  x0 = (x + pi) / 2pi, 0 <= x0 <= 1,
+  y0 = (y + 1) / 2, 0 <= y0 <=1
+*/
+
+/* y = func(x) */
+#define N_SAMPLES 13
+#define N_INPUTS  1
+#define N_OUTPUTS 1
+#define N_HIDDEN  23
+
+#define SUM_ERROR 0.0001
+
+float input_sect[2] = { -PI, PI };
+float output_sect[2] = { -1, 1 };
+
+int n_nodes[] = {
+	N_INPUTS, N_HIDDEN, N_OUTPUTS
+};
+
+void gen_samples(float input[N_SAMPLES][N_INPUTS], float result[N_SAMPLES][N_OUTPUTS]);
+int read_input(float input[N_INPUTS], FILE* in);
+int write_output(float output[N_OUTPUTS], FILE* out);
+
+/* args analyse */
+struct _globalConfig
 {
-	int i;
-	float dim = 1.0 / (N_SAMPLES - 1);
-	for (i = 0; i < N_SAMPLES; i++)
-	{
-		input[i] = in_adj(dim * i);
-		result[i] = out_adj_rev(func(input[i]));
-	}
-}
+	/* mode */
+	int quiet;		/* -q */
+	
+	/* config file names */
+	const char* net_load;	/* -l,-n, default: retrain the net */
+	const char* net_save;	/* -s,-n, default: none */
+	const char* error_output; /* -e, default: none */
+	const char* test_input;	  /* -i, default: stdin */
+	const char* test_output;  /* -o, default: stdout */
+}globalConfig;
+const char* opts_analyse = "qn:l:s:e:i:o:";
+int opts_analyser(int argc, char* argv[]);
 
 int main(int argc, char* argv[])
 {
 	NeuralNet net;
-	int n_nodes[] = {
-		1, 23, 1
-	};
-	float input, error, sumerr;
+	float error, sumerr;
 	int i, n = 0;
-
-	float sample_input[N_SAMPLES];
-	float sample_result[N_SAMPLES];
 	
-	FILE* fl = NULL;
+	float sample_input[N_SAMPLES][N_INPUTS];
+	float sample_result[N_SAMPLES][N_OUTPUTS];
+	float test_input[N_INPUTS];
+	float test_output[N_OUTPUTS];
+
+	FILE *fnet, *ferror, *ftest_in, *ftest_out;
+
+	/* opts analyse */
+	if (opts_analyser(argc, argv) < 0)
+	{
+		perror("opts_anal");
+		return -1;
+	}
 
 	/* init samples */
+	if (!globalConfig.quiet)
+		fprintf(stderr, "loading samples...");
 	gen_samples(sample_input, sample_result);
 
 	/* init net, or load from file */
-	if (argc >= 2 && strcmp(argv[1], "-l") == 0)
+	fnet = NULL;
+	if (globalConfig.net_load && (fnet = fopen(globalConfig.net_load, "r")))
 	{
-		fl = fopen(save_net, "r");
-	}
-
-	if (fl)
-	{
-		readNet(fl, &net);
-		fclose(fl);
+		if (!globalConfig.quiet)
+			fprintf(stderr, "complete\nloading net...");
+		readNet(fnet, &net);
+		fclose(fnet);
 	}
 	else
 	{
@@ -96,39 +119,69 @@ int main(int argc, char* argv[])
 	}
 	
 	/* train */
+	if (!globalConfig.quiet)
+			fprintf(stderr, "complete\ntraining...");
+	ferror = NULL;
+	if (globalConfig.error_output)
+		ferror = fopen(globalConfig.error_output, "w");
 	do
 	{
 		sumerr = 0;
 		for (i = 0; i < N_SAMPLES; i++)
 		{
-			sumerr += evolveNet(&net, &sample_input[i], &sample_result[i]);
+			sumerr += evolveNet(&net, sample_input[i], sample_result[i]);
 		}
-		/* print Generation & errors */
-		fprintf(ERROR_OUTPUT, "%d\t%f\n", n++, sumerr);
+		/* print Generation & Errors */
+		if (ferror)
+			fprintf(ferror, "%d\t%f\n", n++, sumerr);
+		
 	}while(sumerr > SUM_ERROR);
+	if (ferror)
+		fclose(ferror);
 	
 	/* run test */
-	while(fscanf(TEST_INPUT, "%f", &input) == 1)
+	if (!globalConfig.quiet)
+		fprintf(stderr, "complete\nNow start a test:\n");
+
+	if (globalConfig.test_input)
+		ftest_in = fopen(globalConfig.test_input, "r");
+	else
+		ftest_in = stdin;
+
+	if (globalConfig.test_output)
+		ftest_out = fopen(globalConfig.test_output, "w");
+	else
+		ftest_out = stdout;
+
+	while(read_input(test_input, ftest_in) == N_INPUTS)
 	{
-		caculateNet(&net, &input);
-		fprintf(TEST_OUTPUT, "%f\n", out_adj(net.output->result[0]));
+		caculateNet(&net, test_input);
+		for (i = 0; i < N_OUTPUTS; i++)
+		{
+			test_output[i] = out_adj(net.output->result[i]);
+		}
+		write_output(test_output, ftest_out);
 	}
+	if (ftest_in != stdin)
+		fclose(ftest_in);
+	if (ftest_out != stdout)
+		fclose(ftest_out);
 
 	/* print & save Net */
-	writeNet(stdout, &net);
-	if ((fl = fopen(save_net, "w")) != 0)
-		writeNet(fl, &net);
-	else
-		fprintf(stderr, "Error in Save Net.");
+	if (globalConfig.net_save && (fnet = fopen(globalConfig.net_save, "w")))
+	{
+		if (!globalConfig.quiet)
+			fprintf(stderr, "writing net...");
+		writeNet(fnet, &net);
+		fclose(fnet);
+		if (!globalConfig.quiet)
+			fprintf(stderr, "complete\nexit.\n");
+
+	}
 
 	releaseNet(&net);
 	
 	return 0;
-}
-
-float func(float x)
-{
-	return ((sin(x) + cos(x)) / 2);
 }
 
 float adjust(float x, float in_sect[2], float out_sect[2])
@@ -139,3 +192,82 @@ float adjust(float x, float in_sect[2], float out_sect[2])
 		(out_sect[1] - out_sect[0]);
 }
 
+float func(float x)
+{
+	return ((sin(x) + cos(x)) / 2);
+}
+void gen_samples(float input[N_SAMPLES][1], float result[N_SAMPLES][1])
+{
+	int i;
+	float dim = 1.0 / (N_SAMPLES - 1);
+	for (i = 0; i < N_SAMPLES; i++)
+	{
+		input[i][0] = in_adj(dim * i);
+		result[i][0] = out_adj_rev(func(input[i][0]));
+	}
+}
+int read_input(float input[N_INPUTS], FILE* in)
+{
+	int i;
+	for (i = 0; i < N_INPUTS; i++)
+	{
+		if (fscanf(in, "%f", &input[i]) != 1)
+			break;
+	}
+	return i;
+}
+
+int write_output(float output[N_OUTPUTS], FILE* out)
+{
+	int i;
+	for (i = 0; i < N_OUTPUTS; i++)
+	{
+		if (fprintf(out, "%f ", output[i]) < 0)
+			break;
+	}
+	fputc('\n', out);
+	return i;
+}
+
+int opts_analyser(int argc, char* argv[])
+{
+	int opt;
+
+	globalConfig.quiet = 0;
+	globalConfig.net_load = NULL;
+	globalConfig.net_save = NULL;
+	globalConfig.error_output = NULL;
+	globalConfig.test_input = NULL;
+	globalConfig.test_output = NULL;
+	
+	while ((opt = getopt(argc, argv, opts_analyse)) != -1)
+	{
+		switch (opt)
+		{
+		case 'q':
+			globalConfig.quiet = 1;
+			break;
+		case 'n':
+			globalConfig.net_save = globalConfig.net_load = optarg;
+			break;
+		case 'l':
+			globalConfig.net_load = optarg;
+			break;
+		case 's':
+			globalConfig.net_save = optarg;
+			break;
+		case 'e':
+			globalConfig.error_output = optarg;
+			break;
+		case 'i':
+			globalConfig.test_input = optarg;
+			break;
+		case 'o':
+			globalConfig.test_output = optarg;
+			break;
+		default:
+			break;
+		}
+	}
+	return 0;
+}
